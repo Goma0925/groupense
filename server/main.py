c517800154240f96852337177c9d5baf16b7bacd
Goma0925 <>
2021-07-07 10:03:52 -0500

    Fixed CORS options

--- server/main.py
+++ server/main.py
@@ -15,7 +15,13 @@ allow_origins = [
     "http://127.0.0.1:3000",
     "http://localhost:3000"
 ]
-app.add_middleware(CORSMiddleware, allow_origins=allow_origins)
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(root_router)
 app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
